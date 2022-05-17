ca7838b7556f141ea7d3a33c133d93c326d08564
Mijail Q. Mariano <mijail.mariano@gmail.com>
2022-05-17 12:43:53 -0400

    this is a greeting python program for testing GitHub.

---
+++ greeting_program.py
@@ -0,0 +1,7 @@
+# this is a python file example where I will generate some code then...
+# i will 'rename' this file using the 'mv' command on the terminal to more accurately describe this file
+# here we go
+
+name = input('Please enter your name: ')
+greeting = 'Hi there,'
+print(greeting, name + '!')
